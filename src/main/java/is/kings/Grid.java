a332849d1e032426066e613b012dd75ff747f3e2
rikard <rikardarnar@gmail.com>
2014-10-29 17:30:24 +0000

    Added constructor for Grid

--- src/main/java/is/kings/Grid.java
+++ src/main/java/is/kings/Grid.java
@@ -2,4 +2,35 @@ package is.kings;
 
 public class Grid{
 
+	private static Node grid[][];
+	private static int counter;
+
+	private static class Node{
+		private int position;
+		private boolean marked;
+		private char xOrO;
+
+		public Node(int position, boolean marked, char xOrO){
+			this.position = position;
+			this.marked = marked;
+			this.xOrO = xOrO;
+		}
+
+		public int getPosition() { return position; }
+		public boolean getMarked() { return marked; }
+		public char getxOrO() { return xOrO; }
+	}
+
+	public Grid(){
+		grid = new Node[3][3];
+		int p = 1;
+		counter = 0;
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				grid[i][j] = new Node(p, false, '.');
+				p++;
+			}
+		}
+	}
+
 }
