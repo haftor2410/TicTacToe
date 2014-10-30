a32ec363427cd3a04bc2d5f1a72c4af617529a06
rikard <rikardarnar@gmail.com>
2014-10-30 14:49:49 +0000

    Refactoring for computerMove()

--- src/main/java/is/kings/ComputerPlayer.java
+++ src/main/java/is/kings/ComputerPlayer.java
@@ -5,14 +5,22 @@ import java.util.Random;
 public class ComputerPlayer{
 
 	public boolean computerMove(Grid g){
-		Random computerMoving = new Random();
-		int theMove = computerMoving.nextInt(9) + 1;
-		if(!g.insertToGridPosition(theMove, 'O')){
+		int theMove = generateNumber();
+		if(isFieldTaken(g, theMove)){
 			return false;
 		}
 		else{
 			return true;
 		}
+	}
+
+	private boolean isFieldTaken(Grid g, int theMove){
+		return !g.insertToGridPosition(theMove, 'O');
+	}
+
+	private int generateNumber(){
+		Random computerMoving = new Random();
+		return computerMoving.nextInt(9) + 1;
 	}
 
 }
