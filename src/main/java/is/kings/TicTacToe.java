2f14a2a8bd939d67031e51d77bc13059dc6704f8
rikard <rikardarnar@gmail.com>
2014-10-30 15:42:37 +0000

    Modified the class TicTacToe.java and added the main function

--- src/main/java/is/kings/TicTacToe.java
+++ src/main/java/is/kings/TicTacToe.java
@@ -1,5 +1,50 @@
 package is.kings;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class TicTacToe{
+
+	public static void main(String[] args){
+		Grid gameGrid = new Grid();
+		HumanPlayer human = new HumanPlayer();
+		ComputerPlayer computer = new ComputerPlayer();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(!gameGrid.gameConclusion())
+		{
+			String humanInput = "";
+			System.out.println("Please enter a number from 1-9");
+			try{
+				humanInput = br.readLine(); 
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+			while(!human.checkInput(humanInput)){
+				System.out.println("Please enter a number from 1-9");
+				try{
+                                	humanInput = br.readLine(); 
+                        	}
+                        	catch(Exception e){
+                                	e.printStackTrace();
+                        	}
+			}
+
+			if(!human.playerMove(gameGrid, Integer.parseInt(humanInput))){
+				continue;
+			}
+			System.out.println(gameGrid.printGrid());
+			if(gameGrid.gameConclusion()){
+				return;
+			}
+			System.out.println("Computer's turn");
+			while(!computer.computerMove(gameGrid)){
+			}
+			System.out.println(gameGrid.printGrid());
+			if(gameGrid.gameConclusion()){
+				return;
+			}
+		}
+	}
 
 }
