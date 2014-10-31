e770ae4cf88521bf71d601c88b4e33a87cded21e
Haftor Gunnlaugsson <you@example.comhaftor.g@gmail.com>
2014-10-31 08:23:25 +0000

    index.html modified + TicTacToeWeb modified to be able to accept the form coming from the user input

--- src/main/java/is/kings/TicTacToeWeb.java
+++ src/main/java/is/kings/TicTacToeWeb.java
@@ -17,10 +17,13 @@ public class TicTacToeWeb implements SparkApplication{
 
 	public void init(){
 		final Grid grid = new Grid();
+		final HumanPlayer hPlayer = new HumanPlayer();
 
 		post(new Route("/fight"){
 			@Override
 			public Object handle(Request req, Response res){
+				Integer number = Integer.valueOf(req.queryParams("move"));
+				hPlayer.playerMove(grid, number);
 				StringBuilder html = new StringBuilder();
 				html.append("<pre>").append(grid.printGrid()).append("</pre>");
 				String gridOut = html.toString();
